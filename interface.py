831a4645a73ea242e9fb8f9a6ce94c1bfd03e5b8
nata-vito <nata14vitorino@hotmail.com>
2023-07-28 18:52:35 +0000

    Language changed to pt

--- interface.py
+++ interface.py
@@ -10,7 +10,7 @@ class OCR_interface:
 
     def ocr_process(self, img = None):
         
-        ocr = PaddleOCR(use_angle_cls=True)  # need to run only once to download and load the model into memory
+        ocr = PaddleOCR(use_angle_cls=True, lang = 'pt')  # need to run only once to download and load the model into memory
         img_path = img
         result = ocr.ocr(img_path, cls=True)
 
@@ -22,10 +22,10 @@ class OCR_interface:
         # Draw result
         result = result[0]
         image = img #Image.open(img_path).convert('RGB')
-        boxes = [line[0] for line in result]
+        boxes = [line[0] for line in result]  
         txts = [line[1][0] for line in result]
         scores = [line[1][1] for line in result]
-        font_path = "/home/nata-brain/Documents/machine_learning/text_processing/fonts/simfang.ttf"  # Replace this with the path to your preferred TrueType font file.
+        font_path = "/root/OCR-notas/fonts/simfang.ttf"  # Replace this with the path to your preferred TrueType font file.
         im_show = draw_ocr(image, boxes, txts, scores, font_path=font_path)
         
         return im_show, txts
