053b43c9ba8066e4dfca4dc492bf5c7738a0bd51
lucaslco <lucaslco2005@gmail.com>
2023-10-17 18:08:05 -0300

    refactor: pass functions to utils file

--- api.py
+++ api.py
@@ -1,32 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
-from paddleocr import PaddleOCR
-import numpy as np
-import cv2
+from utils import ocr_process, read_imagefile
 
 app = FastAPI()
-
-def ocr_process(img):
-        ocr = PaddleOCR(use_angle_cls=True)                                                  # need to run only once to download and load the model into memory
-        result = ocr.ocr(img, cls=True)
-        return result
-
-def read_imagefile(data):
-    npimg = np.frombuffer(data, np.uint8)
-    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    return frame
 
 @app.get("/")
 def read_root():
     return {"Image API read!"} 
- 
+
 @app.post("/upload/")
 async def create_upload_file(file: UploadFile = File(...)):
     image = read_imagefile(await file.read())
-    result = ocr_process(image)
-    
-    return {"result":f"{result}"}
+    phrases = ["RUA", "TOTAL", "CNPJ"]
+    img_result, txt = ocr_process(image, phrases)
+    print(txt)
+    return {"result":f"{txt}"}
+
 
 if __name__ == "__main__":
     # uvicorn main:app --reload --host "0.0.0.0" --port 8080
