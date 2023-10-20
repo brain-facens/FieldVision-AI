1b32573a9ca21ec6cc9ae93c1bc21e28edbd24ca
nata-vito <nata14vitorino@hotmail.com>
2023-10-20 12:10:55 -0300

    refactor: fix pylint warnings

--- python_scripts/utils.py
+++ python_scripts/utils.py
@@ -1,4 +1,4 @@
-from paddleocr import PaddleOCR
+from paddleocr import PaddleOCR # C0114, pylint: disable=missing-module-docstring
 from typing import List
 import numpy as np
 import cv2
@@ -71,8 +71,8 @@ def list_of_strings(arg):
 def read_imagefile(data):
     """ TO DO """
     npimg = np.frombuffer(data, np.uint8)
-    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR) # E1101, so pylint: disable=invalid-name
-    cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR) # E1101, pylint: disable=no-member
+    cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # E1101, pylint: disable=no-member
     return frame
 
 def ocr_process(img, phrases):
@@ -81,12 +81,18 @@ def ocr_process(img, phrases):
     result = ocr.ocr(img, cls=True)  # Get OCR results for the input image
 
     # Extract relevant information from OCR results based on specified phrases
-    coord = []
-    text_lines = []
-    text_lines_sec = []
-    boxes = []
-    boxes_sec = []
-    txts = []
+    structure_result = {
+        "coord": [],
+        "text_lines": [],
+        "text_lines_sec": [],
+        "boxes": [],
+        "boxes_sec": [],
+        "txts": [], 
+        "num_boxes": None,
+        "all_txts": None,
+        "num_boxes_sec": None
+    }
+
 
     for f in phrases:
         phrase = f.upper().replace(" ", "").replace("$","S")
@@ -97,59 +103,60 @@ def ocr_process(img, phrases):
                 line_plus = _line_plus[: len(phrase)]
 
                 if line_plus == phrase:
-                    coord.append({line[1][0]: line[0]})
-                    text_lines.append(line[1][0])
-                    boxes.append(line[0])
+                    structure_result['coord'].append({line[1][0]: line[0]})
+                    structure_result["text_lines"].append(line[1][0])
+                    structure_result["boxes"].append(line[0])
 
                     if len(phrase) != len(_line_plus):
                         txts_ = line[1][0]
                         txts_ = txts_.split(f)
-                        txts.append(txts_)
+                        structure_result["txts"].append(txts_)
 
         # Process the found coordinates
-        for comp in coord:
-            produto = next(iter(comp.keys()))
-            coordenada = comp[produto]
+        for comp in structure_result['coord']:
+            product = next(iter(comp.keys()))
+            coordinates = comp[product]
 
-            xy_min = coordenada[0]
-            xy_max = coordenada[2]
+            xy_min = coordinates[0]
+            xy_max = coordinates[2]
 
-            print(f"\n\nCOORDENADA DO CAMPO ({produto}) {xy_min, xy_max}\n")
+            print(f"\n\nCOORDENADA DO CAMPO ({product}) {xy_min, xy_max}\n")
             for l in result:
                 for axis in l:
                     _xy_min = axis[0][0]
                     _xy_max = axis[0][2]
 
-                    compara_min = xy_min[1] / _xy_min[1]
-                    compara_max = xy_max[1] / _xy_max[1]
+                    comparing_min = xy_min[1] / _xy_min[1]
+                    comparing_max = xy_max[1] / _xy_max[1]
 
-                    diferente = False
+                    different = False
                     if (
                         xy_min[0] != _xy_min[0]
                         and xy_max[0] != _xy_max[0]
                         and xy_min[0] < _xy_min[0]
                         and xy_max[0] < _xy_max[0]
                     ):
-                        diferente = True
+                        different = True
 
                     if (
-                        (compara_min >= 0.98 and compara_min <= 1.02)
-                        and (compara_max >= 0.98 and compara_max <= 1.02)
-                        and (diferente == True)
-                        and (axis[0] not in boxes)
+                        (comparing_min >= 0.98 and comparing_min <= 1.02)
+                        and (comparing_max >= 0.98 and comparing_max <= 1.02)
+                        and (different == True)
+                        and (axis[0] not in structure_result["boxes"])
                     ):
                         print(
                             f"ACHOU, CAMPO ({axis[1][0]}), Coordenada {_xy_min, _xy_max}"
                         )
-                        text_lines_sec.append(axis[1][0])
-                        boxes_sec.append(axis[0])
+                        structure_result["text_lines_sec"].append(axis[1][0])
+                        structure_result["boxes_sec"].append(axis[0])
 
-        num_boxes = len(boxes)
-        boxes = np.array(boxes).reshape(num_boxes, 4, 2).astype(np.int64)
+        structure_result["num_boxes"] = len(structure_result["boxes"])
+        structure_result["boxes"] = np.array(structure_result["boxes"]).reshape(structure_result["num_boxes"], 4, 2).astype(np.int64)
 
-        num_boxes_sec = len(boxes_sec)
-        boxes_sec = np.array(boxes_sec).reshape(num_boxes_sec, 4, 2).astype(np.int64)
+        structure_result["num_boxes_sec"] = len(structure_result["boxes_sec"])
+        structure_result['boxes_sec'] = np.array(structure_result["boxes_sec"]).reshape(structure_result["num_boxes_sec"], 4, 2).astype(np.int64)
 
-        all_texts = np.concatenate((text_lines, text_lines_sec), axis=0)
+        structure_result['all_txts'] = np.concatenate((structure_result["text_lines"],
+                                    structure_result["text_lines_sec"]), axis=0)
 
-        return all_texts.tolist()
+        return structure_result['all_txts'].tolist()
