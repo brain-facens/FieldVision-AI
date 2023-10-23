d3c64fb7247c35475c9466d2aa720927071bcd3a
nata-vito <nata14vitorino@hotmail.com>
2023-10-23 18:16:57 -0300

    doc: english translation completed

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -144,4 +144,4 @@ async def get_latest_filter():
     return {"latest_filter":filter_words.get_filter()}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8085, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
