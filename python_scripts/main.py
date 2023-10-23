6d6897f8a1064855128f0ff62c58b0aa0eebd012
nata-vito <nata14vitorino@hotmail.com>
2023-10-23 17:24:10 -0300

    docs: added explanation to docstring

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -138,10 +138,10 @@ async def get_latest_filter():
     Args:
 
     Returns:
-        Latest word filter on API.
+        Returns the latest word filter on API.
     """
 
     return {"latest_filter":filter_words.get_filter()}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8085, reload=True)
