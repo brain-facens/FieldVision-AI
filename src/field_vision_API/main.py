971ea7a8ac8081efcee7163b67a388f659a3d268
nata-vito <nata14vitorino@hotmail.com>
2023-10-24 19:27:36 -0300

    buil: added only relevant packages

--- src/field_vision_API/main.py
+++ src/field_vision_API/main.py
@@ -68,7 +68,7 @@ def read_root():
 @app.post("/v1/post_image/")
 async def post_file(file: UploadFile = File(...)):
     """ 
-    POST method to API root/home path.
+    POST method to upload image.
 
     Args:
 
